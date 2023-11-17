b95fc9f11b49f92d5022c66aaa4c3ddb7b532355
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-17 08:08:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your JS code here. If required.
-
+let para = document.getElementById("counter");
+let buttons = document.getElementById("incrementBtn");
+let count=0;
+function func()
+{
+	count++;
+}
+buttons.addEventListener("click",func);
